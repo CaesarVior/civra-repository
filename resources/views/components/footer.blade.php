0c78fac0cbcf34f162fed629f82e7b548014d40e
inorayyosinoya-beep <inorayyosinoya@gmail.com>
2026-06-22 22:48:28 +0700

    Add navbar and footer components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,55 @@
+<footer class="footer">
+    <div class="footer-container">
+
+        <div class="footer-left">
+            <div class="footer-image">
+                <img src="kosong dulu"
+                     alt="gatau logonya dawg">
+            </div>
+
+            <div class="footer-social">
+    <a href="#"><i class="fab fa-facebook"></i></a>
+
+    <a href="#"><i class="fab fa-instagram"></i></a>
+
+    <a href="#"><i class="fab fa-whatsapp"></i></a>
+
+    <a href="#"><i class="fab fa-tiktok"></i></a>
+
+    <a href="#"><i class="fab fa-linkedin"></i></a>
+</div>
+        </div>
+
+        <div class="footer-brand">
+            <h2>Artisantz</h2>
+        </div>
+
+        <div class="footer-links">
+            <div>
+                <h4>UMUM</h4>
+                <a href="#">Beranda</a>
+                <a href="#">Produk</a>
+                <a href="#">Kategori</a>
+            </div>
+
+            <div>
+                <h4>LOREM</h4>
+                <a href="#">Lorem Ipsum</a>
+                <a href="#">Lorem Ipsum</a>
+                <a href="#">Lorem Ipsum</a>
+            </div>
+
+            <div>
+                <h4>LOREM</h4>
+                <a href="#">Lorem Ipsum</a>
+                <a href="#">Lorem Ipsum</a>
+                <a href="#">Lorem Ipsum</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        ©2025 by Kivora Team. All Right Reserved
+    </div>
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+    <div class="navbar-logo">
+        Artisantz
+    </div>
+
+    <ul class="navbar-menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Gallery</a></li>
+    </ul>
+
+    <div class="navbar-contact">
+        <a href="#">Contact</a>
+    </div>
+</nav>
