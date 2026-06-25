5ad976d26b2b030bbaf24d1cab13d59cba3d4bd0
Zura <kvreor1@gmail.com>
2026-06-25 11:43:51 +0700

    final home page

---
+++ resources/views/components/best-seller.blade.php
@@ -0,0 +1,26 @@
+<section class="best-seller">
+
+    <h2 class="best-title">
+        BEST <span>SELLER!</span>
+    </h2>
+
+    <div class="podium-wrapper">
+
+        <div class="rank rank-left">
+            <h3>#2</h3>
+            <img src="{{ asset('images/product2.jpg') }}" alt="">
+        </div>
+
+        <div class="rank rank-center">
+            <h3>#1</h3>
+            <img src="{{ asset('images/product1.jpg') }}" alt="">
+        </div>
+
+        <div class="rank rank-right">
+            <h3>#3</h3>
+            <img src="{{ asset('images/product3.jpg') }}" alt="">
+        </div>
+
+    </div>
+
+</section>
