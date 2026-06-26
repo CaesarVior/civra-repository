d425fb5dae128d551fb46a4aebe7c5503b82f83e
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-06-26 17:18:11 +0700

    Fixing home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,28 @@
-@extends ('app')
-@section('content')
-    <!-- HERO SECTION -->
-    <section class="hero">
+@extends('app')
 
+@section('content')
+    <section class="hero">
         <x-artisantz-logo />
 
         <div class="hero-content">
             <div class="hero-h">
-                <h1>Where Art Meets <br>
-                    Your Daily Brew.</h1>
+                <h1>Where Art Meets <br> Your Daily Brew.</h1>
             </div>
             <div class="hero-p">
                 <p>
                     Lebih dari sekadar tempat ngopi. Cafe Artisantz adalah ruang <br> kreatif bagi para penikmat rasa,
                     pencari inspirasi, <br>dan pemburu estetika di tengah kota.
-
                 </p>
             </div>
         </div>
     </section>
 
-
-    <div class="best-seller">
+    <x-container>
 
         <section class="artisantz-section">
-
             <div class="artisantz-wrapper">
                 <div class="artisantz-left">
-                    <h2>What Is
-                        Artisantz?</h2>
+                    <h2>What Is Artisantz?</h2>
                 </div>
 
                 <div class="artisantz-right">
@@ -42,38 +36,30 @@
                     </p>
                 </div>
             </div>
-
-
         </section>
-        <div class="best-seller">
 
+        <div class="best-seller">
             <h2 class="best-title">
                 BEST <span>SELLER!</span>
             </h2>
 
             <div class="podium-wrapper">
-
-                <!-- Ranking 2 -->
                 <div class="rank rank-left">
                     <h3>#2</h3>
                     <img src="{{ asset('images/kopi2.jpg') }}" alt="Best Seller 2">
                 </div>
 
-                <!-- Ranking 1 -->
                 <div class="rank rank-center">
                     <h3>#1</h3>
                     <img src="{{ asset('images/kopi1.jpg') }}" alt="Best Seller 1">
                 </div>
 
-                <!-- Ranking 3 -->
                 <div class="rank rank-right">
                     <h3>#3</h3>
                     <img src="{{ asset('images/kopi3.jpg') }}" alt="Best Seller 3">
                 </div>
-
             </div>
-
         </div>
 
-        </x-container>
-    @endsection
+    </x-container>
+@endsection
