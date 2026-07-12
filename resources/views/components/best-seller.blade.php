91e5afdaba87ebb9e85af3f65a835def0dae085d
Zura <kvreor1@gmail.com>
2026-07-12 18:20:44 +0700

    add home content

--- resources/views/components/best-seller.blade.php
+++ resources/views/components/best-seller.blade.php
@@ -1,31 +1,32 @@
-<section class="about-section">
+<div class="artisantz-bottom">
 
-    <div class="container">
+    <div class="best-seller">
 
-        <div class="about-wrapper">
+        <h2 class="best-title">
+            BEST <span>SELLER!</span>
+        </h2>
 
-            <div class="about-left">
-                <h2>
-                    What Is <br>
-                    Artisantz?
-                </h2>
+        <div class="podium-wrapper">
+
+            <div class="podium-bg"></div>
+
+            <div class="rank rank-left">
+                <h3>#2</h3>
+                <img src="{{ asset('img/kopi2.webp') }}" alt="">
             </div>
 
-            <div class="about-right">
+            <div class="rank rank-center">
+                <h3>#1</h3>
+                <img src="{{ asset('img/kopi1.webp') }}" alt="">
+            </div>
 
-                <p>
-                    Lorem ipsum dolor sit amet...
-                </p>
-
-                <a href="/about" class="btn-about">
-                    View More
-                </a>
-
+            <div class="rank rank-right">
+                <h3>#3</h3>
+                <img src="{{ asset('img/kopi3.webp') }}" alt="">
             </div>
 
         </div>
 
-
     </div>
 
-</section>
+</div>
