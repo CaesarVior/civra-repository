193a664f4e7e513de8710a868c65b27ee5c97192
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-07-12 17:24:56 +0700

    Fixing conflict

---
+++ resources/views/components/best-seller.blade.php
@@ -0,0 +1,31 @@
+<section class="about-section">
+
+    <div class="container">
+
+        <div class="about-wrapper">
+
+            <div class="about-left">
+                <h2>
+                    What Is <br>
+                    Artisantz?
+                </h2>
+            </div>
+
+            <div class="about-right">
+
+                <p>
+                    Lorem ipsum dolor sit amet...
+                </p>
+
+                <a href="/about" class="btn-about">
+                    View More
+                </a>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</section>
