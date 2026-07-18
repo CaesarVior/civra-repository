5d83f145ce4a6f0cfa23d8b69dc8b68c640d8482
Zura <kvreor1@gmail.com>
2026-07-18 11:47:41 +0700

    add responsive for home page and fix footer on contact

---
+++ resources/views/components/home/container.blade.php
@@ -0,0 +1,79 @@
+<section class="about-section">
+
+    <div class="container">
+
+        <div class="about-wrapper">
+
+            <div class="about-left">
+                <h2>
+                    What Is <br>
+                    Artisantz?
+                </h2>
+            </div>
+
+            <div class="about-right">
+
+                <p>
+                    Artisantz Coffee & Eatery adalah destinasi bagi pecinta kopi dan suasana yang inspiratif. 
+                    Yang mengutamakan kualitas dalam setiap racikan, menghadirkan pelayanan yang ramah, 
+                    serta menciptakan ruang yang nyaman untuk berbagi cerita, berkarya, dan menikmati setiap momen dengan secangkir kopi terbaik.
+                </p>
+
+                <a href="/about" class="btn-about">
+                    View More
+                </a>
+
+            </div>
+
+        </div>
+        <div class="artisantz-bottom">
+
+            <div class="best-seller">
+
+                <h2 class="best-title">
+                    BEST <span>SELLER!</span>
+                </h2>
+
+                <div class="podium-wrapper">
+
+    <div class="podium-bg"></div>
+
+    <div class="rank rank-left">
+        <h3>#2</h3>
+        <img src="{{ asset('img/kopi2.webp') }}"
+             class="drink-btn"
+             data-name="Caramel Latte"
+             data-price="Rp 32.000"
+             data-desc="Espresso dipadukan dengan susu segar dan caramel yang manis."
+             alt="">
+    </div>
+
+    <div class="rank rank-center">
+        <h3>#1</h3>
+        <img src="{{ asset('img/kopi1.webp') }}"
+             class="drink-btn"
+             data-name="Signature Artisantz"
+             data-price="Rp 35.000"
+             data-desc="Minuman andalan Artisantz dengan perpaduan espresso dan cream."
+             alt="">
+    </div>
+
+    <div class="rank rank-right">
+        <h3>#3</h3>
+        <img src="{{ asset('img/kopi3.webp') }}"
+             class="drink-btn"
+             data-name="Matcha Latte"
+             data-price="Rp 30.000"
+             data-desc="Matcha premium dipadukan susu segar dengan rasa yang creamy."
+             alt="">
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    </div>
+</section>
