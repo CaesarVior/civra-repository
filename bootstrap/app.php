4095d652ad64e10a172b8c3277f887af6dfd6f46
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-07-30 04:23:36 +0700

    Fixing: env staging #4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api/v1'
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'login' => LoginMiddleware::class,
         ]);
